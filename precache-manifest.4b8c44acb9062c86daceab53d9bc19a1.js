b01e6d3bc17989720038c90e6964a6aa994b53a7
Edwward Coker <edwardcoker27@gmail.com>
2019-08-20 11:02:28 +0100

    Updates

---
+++ precache-manifest.4b8c44acb9062c86daceab53d9bc19a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9007b1af0bc897466dd4e73baa433933",
+    "url": "%20github.io/Porfolio/index.html"
+  },
+  {
+    "revision": "6580f5f77055a1fb6aff",
+    "url": "%20github.io/Porfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3a00e4a9fa2b2fbed5e3",
+    "url": "%20github.io/Porfolio/static/js/2.cc61a3f3.chunk.js"
+  },
+  {
+    "revision": "6580f5f77055a1fb6aff",
+    "url": "%20github.io/Porfolio/static/js/main.0b429d6a.chunk.js"
+  },
+  {
+    "revision": "1e4d9975420ff110dd79",
+    "url": "%20github.io/Porfolio/static/js/runtime~main.d73d7939.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "%20github.io/Porfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
